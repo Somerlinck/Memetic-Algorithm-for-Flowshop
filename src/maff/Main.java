6c8307e92c576ae3ae4d793d13c9f364918deec4
Thomas Schillaci <thomas.schillaci@gmail.com>
2019-05-03 10:57:24 +0200

    model commit

--- src/maff/Main.java
+++ src/maff/Main.java
@@ -1,9 +1,20 @@
 package maff;
 
+import maff.model.Flowshop;
+import maff.model.Ordonnancement;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Test du NEH :");
+        Flowshop Fs = new Flowshop("res/tai01.txt");
+        new Ordonnancement(Fs.creerListeNEH(5), 5).afficher();
 
+        System.out.println();
+
+        System.out.println("Test du EvaluationSeparation :");
+        Fs = new Flowshop("res/tai01.txt");
+        Fs.EvaluationSeparation();
     }
 
 }
