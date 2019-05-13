c6f2dae53f34f78810de5525a3b89bbdcc23fb22
Somerlinck <leo.vuylsteker@imt-atlantique.net>
2019-05-13 20:18:34 +0200

    LS correction

--- src/maff/operators/LocalSearch.java
+++ src/maff/operators/LocalSearch.java
@@ -1,40 +1,44 @@
 package maff.operators;
 
 import maff.model.Solution;
+import maff.PBSE;
 import maff.model.JobsList;
-import java.util.TreeSet;
+import maff.model.Problem;
 
+import java.util.TreeSet;
 public class LocalSearch extends Operator {
 
     @Override
     public TreeSet<Solution> abstractApply(TreeSet<Solution> solutions) {
     	TreeSet<Solution> neighbors = new TreeSet<>();
     	for(Solution solution : solutions) {
-    		neighbors.add(findBestNeighbor(solution));
+    		Solution best = findBestNeighbor(solution);
+    		best.reset();
+    		neighbors.add(best);
     	}
         return neighbors;
     }
 
-    public Solution findBestNeighbor(Solution solution) {
+    private Solution findBestNeighbor(Solution solution) {
     	Solution best = solution;
     	int length = solution.getSequence().nombreJobs();
     	for(int i = 0 ; i < length-1 ; i++) {
     		for(int j = i+1 ; j < length ; j++) {
     			Solution swap = swap(i,j,solution);
-    			if(swap.getScore() < best.getScore()) best = swap;
+    			if(swap.getScore() < best.getScore()) {best = swap;}
     		}
     	}
 		return best;
     }
 
-    public Solution swap(int i, int j, Solution solution) {
-    	Solution ordo = solution;
-    	JobsList jobs = ordo.getSequence();
-    	Solution res = new Solution(ordo.getNbMachines());
+    private Solution swap(int i, int j, Solution solution) {
+    	JobsList jobs = solution.getSequence();
+    	Solution res = new Solution(solution.getNbMachines());
     	for(int k = 0 ; k < jobs.nombreJobs() ; k++) {
     		if(k==i) res.ajouterJob(jobs.getJob(j));
-    		if(k==j) res.ajouterJob(jobs.getJob(i));
+    		else {if(k==j) res.ajouterJob(jobs.getJob(i));
     		else res.ajouterJob(jobs.getJob(k));
+    		}
     	}
     	return res;
     }
