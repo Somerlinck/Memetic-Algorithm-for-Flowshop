05f02c141e518ddc68b494c6177937a30218462b
Thomas Schillaci <thomas.schillaci@gmail.com>
2019-05-14 08:50:57 +0200

    debug

--- src/maff/operators/Operator.java
+++ src/maff/operators/Operator.java
@@ -7,6 +7,7 @@ import java.util.TreeSet;
 public abstract class Operator {
 
     public TreeSet<Solution> apply(TreeSet<Solution> population) {
+        System.out.println("Size: " + population.size()+"; "+getClass().getSimpleName());
         TreeSet<Solution> unsorted = abstractApply(population);
         unsorted.forEach(Solution::reset);
         TreeSet<Solution> res = new TreeSet<>();
